c919fded57da03fc27842446b287d6ad3fe9cee6
Andrew <andrewz.jackson667@gmail.com>
2018-03-22 22:05:20 -0400

    creaded and added a header image to contact page, created contact-header with text and put a padding around the container and text, changed h3 tag color to cyan

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,51 +1,52 @@
+
 $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+  $('.parallax').parallax();
+});
 // Initialize collapse button
 $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
-      
+    
 $('.dropdown-button1').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrainWidth: false, // Does not change width of dropdown to that of the activator
-    hover: true, // Activate on hover
-    gutter: 0, // Spacing from edge
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    stopPropagation: false // Stops event propagation
-  }
+  inDuration: 300,
+  outDuration: 225,
+  constrainWidth: false, // Does not change width of dropdown to that of the activator
+  hover: true, // Activate on hover
+  gutter: 0, // Spacing from edge
+  belowOrigin: true, // Displays dropdown below the button
+  alignment: 'left', // Displays dropdown with edge aligned to the left of button
+  stopPropagation: false // Stops event propagation
+}
 );
 
 $('.dropdown-button2').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrainWidth: false, // Does not change width of dropdown to that of the activator
-    hover: true, // Activate on hover
-    gutter: 0, // Spacing from edge
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    stopPropagation: false // Stops event propagation
-  }
+  inDuration: 300,
+  outDuration: 225,
+  constrainWidth: false, // Does not change width of dropdown to that of the activator
+  hover: true, // Activate on hover
+  gutter: 0, // Spacing from edge
+  belowOrigin: true, // Displays dropdown below the button
+  alignment: 'left', // Displays dropdown with edge aligned to the left of button
+  stopPropagation: false // Stops event propagation
+}
 );
 $('.dropdown-button3').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrainWidth: false, // Does not change width of dropdown to that of the activator
-    gutter: 0, // Spacing from edge
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    stopPropagation: false // Stops event propagation
-  }
+  inDuration: 300,
+  outDuration: 225,
+  constrainWidth: false, // Does not change width of dropdown to that of the activator
+  gutter: 0, // Spacing from edge
+  belowOrigin: true, // Displays dropdown below the button
+  alignment: 'left', // Displays dropdown with edge aligned to the left of button
+  stopPropagation: false // Stops event propagation
+}
 );
 $('.dropdown-button4').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrainWidth: false, // Does not change width of dropdown to that of the activator
-    gutter: 0, // Spacing from edge
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    stopPropagation: false // Stops event propagation
-  }
+  inDuration: 300,
+  outDuration: 225,
+  constrainWidth: false, // Does not change width of dropdown to that of the activator
+  gutter: 0, // Spacing from edge
+  belowOrigin: true, // Displays dropdown below the button
+  alignment: 'left', // Displays dropdown with edge aligned to the left of button
+  stopPropagation: false // Stops event propagation
+}
 );
